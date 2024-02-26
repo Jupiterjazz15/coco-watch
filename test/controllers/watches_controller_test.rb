188e0a0e1781ce4d49b4821179536fa4286bf95e
coraliehaller <haller.coralie.15@gmail.com>
2024-02-26 14:49:24 +0100

    -mcreation branch watch-controller

---
+++ test/controllers/watches_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class WatchesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get watches_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get watches_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get watches_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get watches_create_url
+    assert_response :success
+  end
+end
