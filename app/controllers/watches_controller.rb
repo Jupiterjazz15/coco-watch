40874247d2b0124d8bc4012a0936d7b3193bc66c
coraliehaller <haller.coralie.15@gmail.com>
2024-02-26 15:06:00 +0100

    ajout des méthodes index / new / show / create / params_watch dans le controller watches

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,13 +1,29 @@
 class WatchesController < ApplicationController
   def index
-  end
-
-  def show
+    @watches = Watch.all
   end
 
   def new
+    @watch = Watch.new
+  end
+
+  def show
+    @watch = Watch.find(params[:id])
   end
 
   def create
+    @watch = Watch(params_watch)
+      if @watch.save
+        redirect_to watch_path(@booking)
+      else
+        render :new, status: :unprocessable_entity
+      end
   end
+
+private
+
+def params_watch
+  params.require(:watch).permit(:brand, :status, :model, :url, :description, :year, :price_per_day, :availabe_for, :available_until, :user_id)
+end
+
 end
