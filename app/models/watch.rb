f71460f30da412f6b27775a5c49686d1cb0a18a3
antoineclavel <clavel.antoine@gmail.com>
2024-02-28 14:06:46 +0100

    end of form

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -2,6 +2,5 @@ class Watch < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :brand, :model, :description, :year, :price_per_day, presence: true
-
   has_one_attached :photo
 end
