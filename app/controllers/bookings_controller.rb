cba2963604b65b9c6dbe4c67eeb27cad4b43190f
coraliehaller <haller.coralie.15@gmail.com>
2024-02-29 16:12:34 +0100

    mesg

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -21,7 +21,7 @@ class BookingsController < ApplicationController
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-    redirect_to dashboard_path, status: :see_other
+    redirect_to dashboard_path(), status: :see_other
   end
 
   private
