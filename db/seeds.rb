c2153853188a2755b78b0987f80b9759bfcc3e9f
coraliehaller <haller.coralie.15@gmail.com>
2024-04-22 19:39:35 +0200

    Modification de la seed en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,32 @@ Booking.destroy_all
 Watch.destroy_all
 User.destroy_all
 
-users_data = [
-  { first_name: "Antoine", last_name: "Lebeau", phone_number: "123-456-7890", avatar: "https://ca.slack-edge.com/T02NE0241-U06DG7195EE-41fa3d1f8d29-512", email: "antoine@test.com", password: "testpassword" },
-  { first_name: "Coralie", last_name: "Haller", phone_number: "234-567-8901", avatar: "https://ca.slack-edge.com/T02NE0241-U06DG28K0CT-fafee26638ca-512", email: "coralie@test.com", password: "testpassword" },
-  { first_name: "Guillaume", last_name: "Dupuis", phone_number: "345-678-9012", avatar: "https://ca.slack-edge.com/T02NE0241-U06D6P7V0UX-2fe19bee1d04-512", email: "guillaume@test.com", password: "testpassword" },
-  { first_name: "Antoine", last_name: "Clavel", phone_number: "456-789-0123", avatar: "https://ca.slack-edge.com/T02NE0241-U06EKJ558NL-g43dd42946ee-512", email: "toto@test.com", password: "testpassword" }
-]
-users_data.each do |user_data|
-  User.create!(user_data)
+user1 = User.create!(first_name: "Antoine", last_name: "Lebeau", phone_number: "123-456-7890", avatar: "https://ca.slack-edge.com/T02NE0241-U06DG7195EE-41fa3d1f8d29-512", email: "antoine@test.com", password: "testpassword")
+p "#{user1.first_name} is created"
 
-end
+user2 = User.create!(first_name: "Coralie", last_name: "Haller", phone_number: "234-567-8901", avatar: "https://ca.slack-edge.com/T02NE0241-U06DG28K0CT-fafee26638ca-512", email: "coralie@test.com", password: "testpassword" )
+p "#{user2.first_name} is created"
+
+user3 = User.create!(first_name: "Guillaume", last_name: "Dupuis", phone_number: "345-678-9012", avatar: "https://ca.slack-edge.com/T02NE0241-U06D6P7V0UX-2fe19bee1d04-512", email: "guillaume@test.com", password: "testpassword")
+p "#{user3.first_name} is created"
+
+user4 = User.create!(first_name: "Antoine", last_name: "Clavel", phone_number: "456-789-0123", avatar: "https://ca.slack-edge.com/T02NE0241-U06EKJ558NL-g43dd42946ee-512", email: "toto@test.com", password: "testpassword")
+p "#{user4.first_name} is created"
+
+watch1 = Watch.create!(
+  brand: "Omega",
+  model: "SEAMASTER AQUA TERRA",
+  description: "La Seamaster Aqua Terra constitue un magnifique hommage au vaste patrimoine maritime d’OMEGA. Le fond de ce modèle de 38 mm, doté d’un boîtier symétrique façonné en acier inoxydable, est orné d’un motif de vagues.",
+  year: "2023",
+  price_per_day: "100",
+  address: "90 Rue Saint-Louis en l'Île, 75004 Paris",
+  user_id: User.first.id
+)
+
+watch1.photo.attach(
+  io: File.open(Rails.root.join('app', 'assets', 'images', 'O1.png')),
+  filename: 'O1.png',
+  content_type: 'image/png'
+)
+
+p "#{watch.model} is created"
